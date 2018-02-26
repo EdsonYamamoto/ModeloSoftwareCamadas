36950da587107f1fe7166bc3d58a0de18b90c466
EdsonYamamoto <edsonkazyamamoto@gmail.com>
2018-02-25 23:29:21 -0300

    123
    qwe

---
+++ ModeloCamadaSoftware/ModeloCamadaSoftware/ValidaCampos/ValidaCampoEdit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloCamadaSoftware.ValidaCampos
+{
+    class ValidaCampoEdit
+    {
+        public String retornaString()
+        {
+            return null;
+        }
+
+        public bool validaBoleano()
+        {
+            return false;
+        }
+
+        public bool validaString()
+        {
+            return false;
+        }
+
+        public bool validaInteger()
+        {
+            return false;
+        }
+    }
+}
